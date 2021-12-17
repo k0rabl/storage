cf3d3082c006636aaee75c2ffcfadc1cadad64ac
k0rabl <k0rab1@yandex.ru>
2021-12-17 13:41:38 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('dbConnect'))
+    
+    app.listen(PORT, () => {
+      console.log('Server start', PORT)
+    })
+  } catch (e) {
+    
+  }
+}
+
+start()
