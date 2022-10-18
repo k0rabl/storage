e914bd2d524ad0fb75eca30262dd83aad6f336ec
k0rabl <theevil228@gmail.com>
2022-10-18 22:40:57 +0500

    Merge handler

---
+++ packages/client/src/features/alert/ui/index.tsx
@@ -0,0 +1,45 @@
+import {FC, useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { clearAlert } from '@features/alert/model/alertSlice';
+import { RootState } from '@store/store';
+
+import classNames from 'classnames';
+
+import styles from './index.module.sass'
+
+
+const mapState = (state: RootState) => ({
+    alert: state.alert,
+})
+
+
+const Alert: FC = () => {
+    const { alert } = useSelector(mapState)
+    const dispatch = useDispatch()
+    const [active, setActive] = useState(false)
+
+    useEffect(() => {
+        if (alert.value.length){
+            setActive(true)
+        }
+
+        if (alert.value.length) {
+            setTimeout(() => {
+                setActive(false)
+                dispatch(clearAlert())
+            }, 3000)
+        }
+    }, [alert.value])
+    
+
+    return (
+        <div className={ classNames([styles.alert, active && styles.alertActive]) }>
+            <div className={ styles.alertText }>
+                { alert.value }
+            </div>
+        </div>
+    )
+}
+
+export default Alert
